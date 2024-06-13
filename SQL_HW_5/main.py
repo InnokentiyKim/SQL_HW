8d78b499dac6145ead7cb4a3d35692f5f5928db6
InnCent <innokentiykim@gmail.com>
2024-06-13 19:22:02 +0500

    HW_5 done

--- SQL_HW_5/main.py
+++ SQL_HW_5/main.py
@@ -1,4 +1,5 @@
 import psycopg2
+import configparser
 from pprint import pprint
 
 
@@ -9,15 +10,15 @@ class Clients:
 
 
     @staticmethod
-    def create_connection(database='', user='', password=''):
+    def create_connection(database='postgres', user='postgres', password='postgres'):
         try:
             conn = psycopg2.connect(database=database, user=user, password=password)
             return conn
         except:
-            "Connection error"
+            print("Connection error")
 
 
-    def create_database(self):
+    def create_database(self) -> None:
         with self.connection.cursor() as curr:
             curr.execute("""
                 DROP TABLE IF EXISTS client CASCADE;
@@ -30,7 +31,7 @@ class Clients:
                     client_id serial PRIMARY KEY,
                     first_name VARCHAR(30) NOT NULL, 
                     last_name VARCHAR(30) NOT NULL,
-                    email VARCHAR(40) NOT NULL  
+                    email VARCHAR(40) NOT NULL
                 ); 
                 """)
             
@@ -56,23 +57,19 @@ class Clients:
             cur_client_id = curr.fetchone()[0]
             if phones:
                 for number in phones:
+                    number = number.replace(' ', '')
                     curr.execute("""
                         INSERT INTO phones(phone_number, client_id)
                         VALUES (%s, %s)
                         """, (number, cur_client_id)
                     )
-            # else:
-            #     curr.execute("""
-            #         INSERT INTO phones(client_id)
-            #         VALUES (%s);
-            #         """, (cur_client_id, )
-            #     )
             self.connection.commit()
-            print(f'Client with id {cur_client_id} added')
+            print(f'Client with id = {cur_client_id} added')
 
 
     def add_phone(self, phone: str, client_id: int):
         with self.connection.cursor() as curr:
+            phone = phone.replace(' ', '')
             curr.execute("""
                 INSERT INTO phones(phone_number, client_id)
                 VALUES (%s, %s);       
@@ -80,7 +77,7 @@ class Clients:
             )
             self.connection.commit()
 
-    def change_clients_info(self, client_id: int, first_name=None, last_name=None, email=None, phones=None):
+    def change_clients_info(self, client_id: int, first_name: str=None, last_name: str=None, email: str=None, phones: list[str]=None):
         with self.connection.cursor() as curr:
             if first_name:
                 curr.execute("""
@@ -104,11 +101,16 @@ class Clients:
                     """, (email, client_id)
                 )
             if phones:
-                for number in phones:
-                    curr.execute("""
-                        UPDATE phones
-                        SET phone_number = %s
+                curr.execute("""
+                        DELETE FROM phones
                         WHERE client_id = %s;                       
+                        """, (client_id, )
+                    )
+                for number in phones:
+                    number = number.replace(' ', '')
+                    curr.execute("""
+                        INSERT INTO phones(phone_number, client_id)
+                        VALUES (%s, %s);                      
                         """, (number, client_id)
                     ) 
             self.connection.commit()
@@ -122,6 +124,19 @@ class Clients:
                 """, (client_id, phone_id)
             )
             self.connection.commit()
+            print(f"Client's (with id = {client_id}) phone number deleted")
+
+
+    def delete_all_phones(self, client_id: int):
+        with self.connection.cursor() as curr:
+            curr.execute("""
+                DELETE FROM phones
+                WHERE client_id = %s
+                """, (client_id, )
+            )
+            self.connection.commit()
+            print(f"All phone numbers deleted for client with id = {client_id}")
+
 
     def delete_client(self, client_id: int):
         with self.connection.cursor() as curr:
@@ -131,8 +146,9 @@ class Clients:
                 """, (client_id, )
             )
             self.connection.commit()
+            print(f"Client with id = {client_id} deleted")
 
-    def find_client(self, first_name=None, last_name=None, email=None, phone=None):
+    def find_client(self, first_name: str=None, last_name=None, email=None, phone: str=None):
         with self.connection.cursor() as curr:
             if not first_name:
                 first_name = '%'
@@ -149,9 +165,10 @@ class Clients:
                 WHERE first_name LIKE %s AND last_name LIKE %s AND email LIKE %s AND phone_number LIKE %s;
                 """, (first_name, last_name, email, phone)
             )
+            print("Found client(s):")
             pprint(curr.fetchall())
 
-    def show_clients_info(self, limit=10):
+    def show_all_clients(self, limit=10):
         with self.connection.cursor() as curr:
             curr.execute("""
                 SELECT *
@@ -162,20 +179,41 @@ class Clients:
             pprint(curr.fetchmany(limit))   
 
 
+    def show_clients_info(self, client_id: int):
+        with self.connection.cursor() as curr:
+            curr.execute("""
+                SELECT *
+                FROM client
+                LEFT JOIN phones using(client_id)
+                WHERE client_id = %s;
+                """, (client_id, )
+            )
+            pprint(curr.fetchone())  
+
+
 def main():
-    conn = Clients.create_connection()
+    config = configparser.ConfigParser()
+    config.read("con_settings.ini")
+    conn = Clients.create_connection(database=config['CNS']['database'], user=config['CNS']['user'], password=config['CNS']['password'])
     client = Clients(conn)
     client.create_database()
     client.add_client('Dmitriy', 'Vasilev', 'dmvasil@mail.ru', ['+7 987 902 33 55', '+7 932 822 11 32'])
     client.add_client('Dmitriy', 'Levin', 'Dlevin@ya.ru', ['+7 934 456 52 13'])
     client.add_client('Sergey', 'Demin', 'SDemin@ya.ru', ['+7 913 355 17 19'])
-    client.add_client('Ivan', 'Ivanov', 'VIvanvo@gmail.com')
+    client.add_client('Ivan', 'Ivanov', 'IvanovI@gmail.com')
     client.add_phone('+7 932 211 12 63', 2)
     client.add_phone('+7 978 434 07 05', 3)
     client.delete_phone(2, 3)
-    client.find_client('Dmitriy', 'Vasilev', 'dmvasil@mail.ru', '+7 987 902 33 55')
+    client.find_client(first_name='Dmitriy', email='dmvasil@mail.ru')
     client.change_clients_info(1, first_name='Grigoriy', email='DGrigory@indox.ru')
-    client.show_clients_info()
+    client.add_client('Ivan', 'Ivanov', 'IvanovI_88@gmail.com', ['+7987 902 3355'])
+    client.change_clients_info(4, phones=['+7 956 124 78 58', '+7 926 259 62 48'])
+    client.show_clients_info(4)
+    client.delete_client(3)
+    client.delete_phone(4, 7)
+    client.delete_all_phones(4)
+    client.show_all_clients()
+    client.find_client(phone='+79322111263')
     conn.close()
 
 if __name__ == '__main__':
