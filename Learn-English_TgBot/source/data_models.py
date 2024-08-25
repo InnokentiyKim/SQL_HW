b0d987f25fe8c6ccf81902f0480b8678992a4120
InnCent <innokentiykim90@gmail.com>
2024-08-25 23:24:00 +0500

    Pydantic models added

--- Learn-English_TgBot/source/data_models.py
+++ Learn-English_TgBot/source/data_models.py
@@ -2,25 +2,36 @@ from typing import Optional
 from pydantic import BaseModel, PositiveInt
 
 
-# class BotUser(BaseModel):
-#     id: Optional[PositiveInt]
-#     name: str
-#     number_of_attempts: Optional[PositiveInt]
-#     successful_attempts: Optional[PositiveInt]
-#     success_streak: Optional[PositiveInt]
-#
-#
-# class Word(BaseModel):
-#     id: Optional[PositiveInt]
-#     eng_title: str
-#     rus_title: str
-#     is_studied: Optional[PositiveInt]
-#     number_of_attempts: Optional[PositiveInt]
-#     successful_attempts: Optional[PositiveInt]
-#     success_streak: Optional[PositiveInt]
-#     user_id: [PositiveInt]
-#
-#
+class BotUser(BaseModel):
+    id: Optional[PositiveInt]
+    name: str
+
+
+class UserSettings(BaseModel):
+    id: Optional[PositiveInt]
+    notification: Optional[PositiveInt]
+    translation_mode: Optional[PositiveInt]
+
+
+class UserStats(BaseModel):
+    id: Optional[PositiveInt]
+    number_of_attempts: Optional[PositiveInt]
+    successful_attempts: Optional[PositiveInt]
+    success_streak: Optional[PositiveInt]
+
+
+class Word(BaseModel):
+    id: Optional[PositiveInt]
+    eng_title: str
+    rus_title: str
+    is_studied: Optional[PositiveInt]
+    number_of_attempts: Optional[PositiveInt]
+    successful_attempts: Optional[PositiveInt]
+    success_streak: Optional[PositiveInt]
+    user_id: [PositiveInt]
+
+
+
 # class Category(BaseModel):
 #     id: Optional[PositiveInt]
 #     eng_name: str
